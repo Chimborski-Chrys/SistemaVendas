cf7b21ef90102695c970b083a27d5eb4422156ea
Chrystiomar <chrystiomar@hotmail.com>
2021-07-10 07:40:56 -0300

    Implementacao das entidades

---
+++ SistemaVendas/Entity/Venda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Entity
+{
+    public class Venda
+    {
+        [Key]
+        public int? Codigo { get; set; }
+        public DateTime Data { get; set; }
+        [ForeignKey("Cliente")]
+        public int CodigoCliente { get; set; }
+        public decimal Total { get; set; }
+        public Cliente Cliente { get; set; }
+
+
+    }
+}
---
+++ SistemaVendas/Entity/VendaProdutos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Entity
+{
+    public class VendaProdutos
+    {
+        public int CodigoVenda { get; set; }
+        public int CodigoProduto { get; set; }
+        public double Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal { get; set; }
+
+        public Produto Produto { get; set; }
+        public Venda Venda { get; set; }
+
+    }
+}
